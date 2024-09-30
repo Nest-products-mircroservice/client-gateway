95d9410da84e8cd0a88d9d848c926a5845ad611b
Pablo <juanpablo310446@gmail.com>
2024-09-30 12:06:10 -0500

    Stiles

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,12 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    })
+    }),
   );
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
-
 
   logger.log(`Application-gateway listening on port ${envs.port}`);
 }
