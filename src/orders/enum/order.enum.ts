24533cd12518db9672d44eb968a7547a2b1105ae
Pablo <juanpablo310446@gmail.com>
2024-10-16 10:02:17 -0500

    production docker file has been created

--- src/orders/enum/order.enum.ts
+++ src/orders/enum/order.enum.ts
@@ -2,6 +2,7 @@ export enum OrderStatus {
   PENDING = 'PENDING',
   DELIVERED = 'DELIVERED',
   CANCELLED = 'CANCELLED',
+  PAID = 'PAID',
 }
 
-export const OrdersStatusList = [OrderStatus.CANCELLED, OrderStatus.DELIVERED, OrderStatus.PENDING];
+export const OrdersStatusList = [OrderStatus.CANCELLED, OrderStatus.DELIVERED, OrderStatus.PENDING, OrderStatus.PAID];
